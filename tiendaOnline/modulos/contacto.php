b88b1219ea19c5b833d93154bc5d484e815df0e2
AlejandroVillarreal <alexvodark@hotmail.com>
2019-10-31 13:14:52 -0600

    Add files via upload

---
+++ tiendaOnline/modulos/contacto.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+      <link rel="stylesheet" href=".\bootstrap\css\bootstrap.css"/>
+      <link rel="stylesheet" href=".\fontawesome\css\all.css"/>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="style.css" />
+    <title>PearStore</title>
+  </head>
+  <body>
+
+
+    <main data-router-wrapper>
+      <section data-router-view="home" class="home">
+        <div class="home-content">
+          <div class="home-presentation">
+            <h1 class="contacto">Integrantes del Equipo</h1>
+            <p></p>
+            <ul class="contacto" style="list-style-type:circle;">
+                <li> Alejandro Daniel Villarreal Ocañas</li><br/>
+                <li> Jairo Ivan Arredondo Gomez</li><br/>
+                <li> Jesus Fernando Hernandez Degollado</li><br/>
+                <li> Alberto Daniel Coronado Arredonto</li><br/>
+              </ul>
+          </div>
+
+        </div>
+      </section>
+    </main>
+
+    <script src="./js/index.js"></script>
+  </body>
+</html>
